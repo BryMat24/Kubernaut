e8f2c6ee0b4dcdd18544058293c48f35f095dd5c
bryan <bryanmatthew206@gmail.com>
2026-07-13 14:53:06 +0700

    delete repo_utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,10 @@
-from .git_utils import get_diff_content, get_changed_files, slugify, open_pull_request
-from .repo_utils import ensure_base_clone, create_task_worktree, remove_task_worktree, repo_lock
+from .git_utils import (
+    get_diff_content,
+    get_changed_files,
+    slugify,
+    open_pull_request,
+    ensure_base_clone,
+    create_task_worktree,
+    remove_task_worktree,
+    repo_lock,
+)
